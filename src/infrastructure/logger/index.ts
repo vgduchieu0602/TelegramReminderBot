f81594a944b9d85a18fe9190ac2479d236beab7d
Vuong Duc Hieu <vg.dhieu0602@gmail.com>
2026-07-02 14:14:31 +0700

    fear: logger

---
+++ src/infrastructure/logger/index.ts
@@ -0,0 +1,36 @@
+import winston from 'winston'
+import path from 'path'
+
+const logFormat = winston.format.combine(
+    winston.format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.errors({ stack: true}),
+    winston.format.json()
+)
+
+const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || 'info',
+    format: logFormat,
+    transports: [
+        new winston.transports.File({
+            filename: path.join('logs', 'error.log'),
+            level: 'error',
+            maxsize: 10485760, // 10MB
+            maxFiles: 5
+        })
+    ]
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        })
+    )
+}
+
+export default logger
